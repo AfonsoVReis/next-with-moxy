e815fc3908bc4c496e901c319b42c9a99d924bad
André Cruz <andremiguelcruz@msn.com>
2020-02-07 11:31:32 +0000

    chore: make it work correctly

--- www/shared/components/page-switcher/PageSwitcher.js
+++ www/shared/components/page-switcher/PageSwitcher.js
@@ -1,7 +1,9 @@
 import React, { useCallback, useEffect, useMemo } from 'react';
 import { SwitchTransition } from 'react-transition-group';
+import { wrap } from 'lodash';
 import ScrollBehavior from 'scroll-behavior';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 import StateStorage from './StateStorage';
 
 const RenderPropTransition = ({ render, onRestoreScroll, onExited, ...props }) => {
@@ -15,10 +17,58 @@ const RenderPropTransition = ({ render, onRestoreScroll, onExited, ...props }) =
     return render({ ...props, onExited: finalOnExited });
 };
 
+const createKey = () => Math.random()
+    .toString(36)
+    .substr(2, 8);
+
+const wrapRouter = (router) => {
+    if (typeof window === 'undefined') {
+        return;
+    }
+
+    Link.defaultProps = {
+        ...Link.defaultProps,
+        scroll: false,
+    };
+
+    history.scrollRestoration = 'manual';
+
+    history.pushState = wrap(history.pushState, (pushState, state, title, url) => {
+        if (state && history.state?.as !== url) {
+            console.log('!!!!!!!!!!!!! pushState new key');
+            state.locationKey = createKey();
+            location.key = state.locationKey;
+        }
+
+        pushState.call(history, state, title, url);
+    });
+
+    history.replaceState = wrap(history.replaceState, (replaceState, state, title, url) => {
+        if (state && history.state?.as !== url) {
+            console.log('!!!!!!!!!!!!! replaceState new key');
+            state.locationKey = createKey();
+            location.key = state.locationKey;
+        }
+
+        replaceState.call(history, state, title, url);
+    });
+
+    router.beforePopState = wrap(router.beforePopState, (beforePopState, fn) => {
+        fn = wrap(fn, (fn, state) => {
+            console.log('>>>', state);
+            location.key = state.locationKey;
+
+            return fn(state);
+        });
+
+        return beforePopState.call(router, fn);
+    });
+
+    router.beforePopState(() => true);
+};
+
 const PageSwitcher = ({ Component, pageProps, router, children }) => {
     const pageKey = router.route;
-
-    console.log('page key', pageKey, StateStorage);
 
     const scrollBehavior = useMemo(() => typeof window !== 'undefined' && new ScrollBehavior({
         addTransitionHook: (callback) => {
@@ -32,11 +82,14 @@ const PageSwitcher = ({ Component, pageProps, router, children }) => {
         stateStorage: new StateStorage(router),
     }), [router]);
 
+    useEffect(() => {
+        wrapRouter(router);
+    }, [router]);
+
     useEffect(() => () => scrollBehavior.stop(), [scrollBehavior]);
 
     const handleRestoreScroll = useCallback(() => {
-        console.log('should restore scroll');
-        scrollBehavior.updateScroll();
+        scrollBehavior.updateScroll(location);
     }, [scrollBehavior]);
 
     return (
